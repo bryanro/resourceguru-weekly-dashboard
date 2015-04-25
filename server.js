e9b6385c8f2de323503b174e61c8eaf806a86739
Bryan Rosenbaum <bryan.rosenbaum@gmail.com>
2015-04-25 16:13:15 -0400

    Handle openshift

--- server.js
+++ server.js
@@ -14,9 +14,19 @@ logger.level = config.loggingVerbosity;
 
 app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
-    logger.info('Express server listening on port ' + server.address().port);
-});
+// handle for openshift
+if (process.env.OPENSHIFT_NODEJS_IP) {
+    var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+    var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+    var server = app.listen(port, ipaddress, function() {
+        logger.info('Express server listening on port ' + server.address().port);
+    });
+}
+else {
+    var server = app.listen(app.get('port'), function() {
+        logger.info('Express server listening on port ' + server.address().port);
+    });
+}
 
 require('./app/express-settings')(app);
 require('./app/server/express-routes')(app);
